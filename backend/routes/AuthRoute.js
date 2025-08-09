da3552b8c4715e9c3781b1fce4e0da81d5bd342c
VIPIN KUMAR <143281757+Vipin9794@users.noreply.github.com>
2025-08-09 23:25:28 +0530

    Update AuthRoute.js

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -8,7 +8,7 @@ const { verifyToken } = require("../middleware/verifyToken");
 // const { UserVerification } = require("../middlewares/UserVerification");
 const router = require("express").Router();
 
-router.post("/signup", verifyToken, Signup);
+router.post("/signup", Signup);
 router.post("/login", verifyToken, login);
 router.get("/getuser", verifyToken, getuser);
 router.get("/logout", logout);
